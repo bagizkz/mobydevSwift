a13522540fd86d655f6b87d29ff0570c34c00435
bagizkz <or.baglane@gmail.com>
2024-08-01 01:40:39 +0500

    добавлена лабораторная работа 2

---
+++ Lab_2.playground/Contents.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+
+// Задание 1
+//Константа не изменяемое значание, вернулся и применил переменную - var
+var friends = 1200
+
+// Удаляем друга
+friends = 1199
+
+// Выводим результат
+print (friends)
+
+
+
+// Задание 2
+
+let goalSteps = 10000
+
+
+
+// Задание 3
+
+var schooling = 11
+print(schooling)
+
+schooling = 12
+print(schooling)
+
+print ("Переменная (var) - может менят присвоенное ранее значение. А константы (let) - не могут менят значение. ")
+
+
+// Задание 4
+
+var steps = 0
+print (steps)
+steps = 2000
+print (steps)
+print ("Хорошая работа! Вы уже на пути к своей ежедневной цели")
+
+
+// Задание 5
+
+var name: String
+//print (name)
+name = "Baglan"
+print (name)
+print ("Впервом случае была ошибка потому что значение переменной name была пустой. Я закомментировал print, дал значение переменой name и это сработало")
+
+
+
+// Задание 6
+var distanceTraveled: Float = 0  // Вернулся и установил тип Float
+distanceTraveled = 54.3
+print (distanceTraveled)  // прверяю
